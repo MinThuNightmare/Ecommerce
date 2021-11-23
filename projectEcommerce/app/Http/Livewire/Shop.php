57a22eb7c66b951696390ddc02dc09f57da00eb9
PHPProjectHome <thureinforex@gmail.com>
2021-11-24 05:38:30 +0630

    showIndexAndCreateTableIndex

--- projectEcommerce/app/Http/Livewire/Shop.php
+++ projectEcommerce/app/Http/Livewire/Shop.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Shop extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.shop')->layout('layouts.base');
+       $products =  Product::paginate(12);
+        return view('livewire.shop', ['products' => $products])->layout('layouts.base');
     }
 }
